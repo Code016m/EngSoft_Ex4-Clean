b600798bf43c03fe25e03269b6a24e180b41cc86
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-04-28 15:35:41 -0300

    Add Livro class with attributes and initializer

---
+++ livraria/core/entities/livro.py
@@ -0,0 +1,7 @@
+class Livro:
+
+    def __init__(self, titulo, autor, categoria, preco):
+        self.titulo = titulo
+        self.autor = autor
+        self.categoria = categoria
+        self.preco = preco
